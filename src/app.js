f135c67a7c8fad834fea74032e92f39b7391e3d2
Büşra <busraoran043@gmail.com>
2022-09-15 17:47:47 +0300

    First Lesson

--- src/app.js
+++ src/app.js
@@ -1,36 +1,20 @@
-console.log("Merhaba Kodlama.io")
-
-//JS type safe değildir
-let dolarBugun = 9.30
+console.log("merhaba js")
+ 
 
 let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
+
+let dolarBugun = 9.30
+
 
 console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
-
-//array
+//arrays
 //camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
+let konutKredileri = ["ev konut kredisi", "dükkan konut kredisi", "kamu konut kredisi","özel konut kredisi"]
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
-}
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
+for (let index = 0; index < konutKredileri.length; index++) {
+    
+    console.log("<li>" + konutKredileri[index] + "</li>")
+} 
